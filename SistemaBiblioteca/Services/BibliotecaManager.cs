e2a0eb1e6d877592a7d126dccf73ab68854f481d
Valentinacha <valentina.chavez2003@gmail.com>
2026-03-11 03:24:09 -0600

    Se agrego la logica del sistema

--- SistemaBiblioteca/Services/BibliotecaManager.cs
+++ SistemaBiblioteca/Services/BibliotecaManager.cs
@@ -5,4 +5,4 @@ public class BibliotecaManager
     public List<Libro> libros = new List<Libro>();
     public List<Usuario> usuarios = new List<Usuario>();
     public List<Prestamo> prestamos = new List<Prestamo>();
-}
+}  
